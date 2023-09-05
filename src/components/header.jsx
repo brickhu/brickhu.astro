7676db3b3c6aa9d0c9837ce7aadf3e43670439b1
Brickhu <brickhu@126.com>
2023-09-05 09:07:44 +0800

    修改header组件

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,18 +6,18 @@ import ThemeSawp from "./theme-swap";
 export default function Header() {
   return (
     <div class="sticky top-0 z-30 ">
-      <header class="navbar bg-opacity-90 backdrop-blur transition-all w-full">
-        <div class="flex-1">
-          <a class="btn btn-ghost normal-case text-xl" href="/">{Site.name}</a>
+      <header className="navbar bg-opacity-90 backdrop-blur transition-all w-full">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl" href="/">{Site.name}</a>
         </div>
-        <div class="flex-none">
-          <ul class="menu menu-horizontal px-1 invisible md:visible">
+        <div className="flex-none">
+          <ul className="menu menu-horizontal px-1 invisible md:visible">
             {Site.menus.map((i)=>(
-              <li><a href={i.path}>{i.name}</a></li>
+              <li key={i.path}><a href={i.path}>{i.name}</a></li>
             ))}
           </ul>
-          <button class="btn btn-square btn-ghost">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+          <button className="btn btn-square btn-ghost">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
           </button>
           <ThemeSawp></ThemeSawp>
         </div>
